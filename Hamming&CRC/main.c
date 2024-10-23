aba8e9a1c622c3f5c291273fa04fa740bc1756b9
dirrectZ <matei.robescu@gmail.com>
2024-10-23 16:42:36 +0300

    First commit

--- Hamming&CRC/main.c
+++ Hamming&CRC/main.c
@@ -1,14 +1,10 @@
-//
-//  main.c
-//  Hamming&CRC
-//
-//  Created by Matei Robescu on 23.10.2024.
-//
-
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    
+    printf("Test");
+    
+    
     return 0;
 }
